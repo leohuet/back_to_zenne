357dd07ca2729daaa203a3c05e6d7761d0373aa0
leohuet <leho-huet@hotmail.fr>
2025-11-05 21:09:22 +0100

    [ADD] Stations channels test retrieving data + OSC send

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import threading
 import time
+from datetime import datetime, timedelta
 import random
 import json
 import os
@@ -33,36 +34,63 @@ local_config = {
 
 # Informations d'authentification
 CID = '9D9E9DE1F0E437A6'
-date = '202401010000'
-date_end = '202501010000'
+presentday = datetime.now()
+format_presentday = presentday.strftime('%Y%m%d')
+yesterday = (presentday - timedelta(1)).strftime('%Y%m%d')
+date = f'{yesterday}0000'
+date_end = f'{format_presentday}0000'
 # --- Sites & canaux ---
 sites = [
     {
         "name": "drogenbos",
         "uid": "4A5190B93E170A87",
         "histdata": "histdata0",
-        "channel": "flowrate"
+        "channel": '"level"'
+    },
+    {
+        "name": "quaidaa",
+        "uid": "9DD946B760E34493",
+        "histdata": "histdata0",
+        "channel": '"ch1"'
+    },
+    {
+        "name": "veterinaires",
+        "uid": "4A26A91BCA0FE58C",
+        "histdata": "histdata0",
+        "channel": '"xch4"'
     },
     {
         "name": "buda",
         "uid": "A35E8E5A539949A7",
         "histdata": "histdata5",
-        "channel": "ch0"
+        "channel": '"ch0"'
     },
     {
-        "name": "elia",
-        "uid": "9DB489F8C6DD874A",
-        "histdata": "histdata5",
-        "channel": "ch4"
-    }
+        "name": "senneOUT",
+        "uid": "4B845F9C7151AC54",
+        "histdata": "histdata0",
+        "channel": '"temp", "conduct", "ph"'
+    },
+
 ]
 
-size_mean = 10
+data_names = {
+    "drogenbos": ["level", "level", "level"],
+    "quaidaa": ["level", "level", "level"],
+    "veterinaires": ["oxygen", "level", "level"],
+    "buda": ["flowrate", "level", "level"],
+    "senneOUT": ["temperature", "acidity", "conductivity"]
+
+}
+
+size_mean = 2
 drogenbos_for_mean = []
+quaidaa_for_mean = []
 pluie_for_mean = []
 for i in range(size_mean):
-    drogenbos_for_mean.append(0)
-    pluie_for_mean.append(0)
+    drogenbos_for_mean.append(0.5)
+    quaidaa_for_mean.append(0.5)
+    pluie_for_mean.append(0.5)
 
 
 def moyenne_glissante(data_array, new_value):
@@ -124,32 +152,45 @@ def save_config(config_dict):
 
 # === THREAD OSC ===
 def osc_sender():
-    global td_client, ableton_client, old_getmtime, local_config
+    global td_client, ableton_client, old_getmtime, local_config, df
     print("OSC thread démarré.")
+    time_index = 0
+    print("sending first hour")
     while True:
         if os.path.getmtime(CONFIG_FILE) != old_getmtime:
             old_getmtime = os.path.getmtime(CONFIG_FILE)
             config = load_config()  # recharge les paramètres à chaque boucle
 
-            osc_address = config["osc_address"]
+            osc_address1 = config["osc_address1"]
+            osc_address2 = config["osc_address2"]
             min_val = float(config["min_value"])
             max_val = float(config["max_value"])
 
             local_config = config
         else:
-            osc_address = local_config["osc_address"]
+            osc_address1 = local_config["osc_address1"]
+            osc_address2 = local_config["osc_address2"]
             min_val = float(local_config["min_value"])
             max_val = float(local_config["max_value"])
 
-        rand = random.randint(0, 100)
-        newrand = ((rand / 100) * (max_val - min_val) + min_val) / 100
-        newrand = moyenne_glissante(drogenbos_for_mean, newrand)
+        df_drogenbos = dataframes.get("drogenbos")
+        df_quaidaa = dataframes.get("quaidaa")
 
-        ableton_client.send_message(osc_address, newrand)
-        td_client.send_message(osc_address, newrand)
+        drogenbos_level_data = df_drogenbos['level'][time_index]
+        drogenbos_mapped_data = (drogenbos_level_data * (max_val - min_val) + min_val) / 100
+        drogenbos_mapped_data = moyenne_glissante(drogenbos_for_mean, drogenbos_mapped_data)
 
-        print(f"Envoi OSC {osc_address}: {newrand}")
-        time.sleep(0.5)  # délai entre envois
+        quaidaa_level_data = df_quaidaa['level'][time_index]
+        quaidaa_mapped_data = (quaidaa_level_data * (max_val - min_val) + min_val) / 100
+        quaidaa_mapped_data = moyenne_glissante(quaidaa_for_mean, quaidaa_mapped_data)
+
+        ableton_client.send_message(osc_address1, drogenbos_mapped_data)
+        ableton_client.send_message(osc_address2, quaidaa_mapped_data)
+
+        print(f"Envoi OSC {osc_address1}: {drogenbos_mapped_data}")
+        print(f"Envoi OSC {osc_address2}: {quaidaa_mapped_data}")
+        time.sleep(1)
+        time_index += 1
 
 
 # === INTERFACE FLASK ===
@@ -185,8 +226,8 @@ if __name__ == "__main__":
 
     # --- Récupération des données pour chaque site ---
     for site in sites:
-        url = f'https://www.flowbru.eu/api/1/customers/{CID}/sites/{site["uid"]}/{site["histdata"]}?json={{"select":["{site["channel"]}"],"from":"{date}","until":"{date_end}"}}'
-
+        url = f'https://www.flowbru.eu/api/1/customers/{CID}/sites/{site["uid"]}/{site["histdata"]}?json={{"select":[{site["channel"]}],"from":"{date}","until":"{date_end}"}} '
+        print(url)
         response = requests.get(url, headers=my_headers)
 
         if response.status_code == 200:
@@ -199,22 +240,50 @@ if __name__ == "__main__":
                     df = df.drop(columns=['t'])  # On garde que "timestamp", plus lisible
                 # Renommer la colonne de valeur pour qu’elle corresponde au site
                 value_col = site["channel"]
-                df.rename(columns={1: "flow_m3s"}, inplace=True)
                 df.rename(columns={0: "date"}, inplace=True)
+                for i in range(1, len(df.columns)):
+                    df[i] = pd.to_numeric(df[i], errors='coerce')
+                    df = df[df[i].notna()]
+                    df[i] = df[i].round(2)
+                df.rename(columns={1: data_names[site['name']][0]}, inplace=True)
+                df.rename(columns={2: data_names[site['name']][1]}, inplace=True)
+                df.rename(columns={3: data_names[site['name']][2]}, inplace=True)
                 # Ajouter le nom du site en colonne (optionnel mais utile pour concat)
                 df['site'] = site['name']
-                # Forcer la conversion en float
-                df['flow_m3s'] = pd.to_numeric(df['flow_m3s'], errors='coerce')
-                # Nettoyage du champ flow_m3s
-                df['flow_m3s'] = df['flow_m3s'].astype(str).str.replace(',', '.', regex=False)
-                df['flow_m3s'] = pd.to_numeric(df['flow_m3s'], errors='coerce')
-                # Supprimer les lignes vides ou NaN
-                df = df[df['flow_m3s'].notna()]
-                # Conversion spécifique pour Drogenbos
-                df['flow_m3s'] = df.apply(
-                    lambda row: row['flow_m3s'] / 1000 if row['site'] == 'drogenbos' else row['flow_m3s'], axis=1)
-                # Arrondi à 3 décimales
-                df['flow_m3s'] = df['flow_m3s'].round(2)
+                """if value_col == "flowrate":
+                    # Forcer la conversion en float
+                    df['flow_m3s'] = pd.to_numeric(df['flow_m3s'], errors='coerce')
+                    # Nettoyage du champ flow_m3s
+                    df['flow_m3s'] = df['flow_m3s'].astype(str).str.replace(',', '.', regex=False)
+                    df['flow_m3s'] = pd.to_numeric(df['flow_m3s'], errors='coerce')
+                    # Supprimer les lignes vides ou NaN
+                    df = df[df['flow_m3s'].notna()]
+                    # Conversion spécifique pour Drogenbos
+                    df['flow_m3s'] = df.apply(
+                        lambda row: row['flow_m3s'] / 1000 if row['site'] == 'drogenbos' else row['flow_m3s'], axis=1)
+                    # Arrondi à 3 décimales
+                    df['flow_m3s'] = df['flow_m3s'].round(2)
+                elif site['name'] == "drogenbos":
+                    df['level'] = pd.to_numeric(df['level'], errors='coerce')
+                    df = df[df['level'].notna()]
+                    df['level'] = df['level'].round(2)
+                    df['level'] = (df['level'] - min(df['level'])) / (max(df['level']) - min(df['level']))
+                elif site['name'] == "viangros":
+                    df['temperature'] = pd.to_numeric(df['temperature'], errors='coerce')
+                    df = df[df['temperature'].notna()]
+                    df['temperature'] = df['temperature'].round(2)
+                    df['ph'] = pd.to_numeric(df['ph'], errors='coerce')
+                    df = df[df['ph'].notna()]
+                    df['ph'] = df['ph'].round(2)
+                    df['conductivity'] = pd.to_numeric(df['conductivity'], errors='coerce')
+                    df = df[df['conductivity'].notna()]
+                    df['conductivity'] = df['conductivity'].round(2)
+                elif site["name"] == "quaidaa":
+                    df['level'] = pd.to_numeric(df['level'], errors='coerce')
+                    df = df[df['level'].notna()]
+                    df['level'] = df['level'].round(2)
+                    df['level'] = (df['level'] - min(df['level'])) / (max(df['level']) - min(df['level']))"""
+
                 # Correction des dates
                 df['date'] = df['date'].apply(corriger_date_brute)
                 df = df[df['date'].notna()]
